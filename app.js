17775e5e4f7edc22df96df58fa00312ebb084c2e
Dave Hernandez <brodave318@gmail.com>
2017-09-06 12:04:13 -0700

    Added a README file and a To-do list in the JS file

---
+++ app.js
@@ -0,0 +1,9 @@
+/* TO-DO LIST ------------------------------------------
+  1. Add event handler >>>>>>>>>>>>>>> CONTROLLER MODULE
+  2. Get inout values >>>>>>>>>>>>>>>>>>>>>>>> UI MODULE
+  3. Add the new item to our data structure >> UI MODULE
+  4. Add the new item to the UI >>>>>>>>>>>> DATA MODULE
+  5. Calculate budget >>>>>>>>>>>>>>>>>>>>>> DATA MODULE
+  6. Update the UI >>>>>>>>>>>>>>>>>>>>>>>>>>> UI MODULE 
+  ----------------------------------------------------*/
+

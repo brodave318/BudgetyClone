8988afe5db9c216c80d9bd52e781cd6da2949004
Dave Hernandez <brodave318@gmail.com>
2017-09-14 16:49:05 -0700

    Created an initialiation function and setup an event listener function

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var UIController = (function() {
   };
   
   return {
-    getiInput: function() {
+    getInput: function() {
       return {
 		type: document.querySelector(DOMstrings.inputType).value, // Will be iether inc or exp
 		description: document.querySelector(DOMstrings.inputDescription).value,
@@ -38,11 +38,21 @@ var UIController = (function() {
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
-  var DOM = UICtrl.getDOMstrings();
   
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+    
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function(event) {
+      if(event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+    
   var ctrlAddItem = function() {
     // 1. Get the field input data
-    var input = UIController.getiInput();
+    var input = UIController.getInput();
     console.log(input);
     // 2. Add the item to the budget controller
     
@@ -51,11 +61,15 @@ var controller = (function(budgetCtrl, UICtrl){
     // 4. Calculate the budget
     
     // 5. Display the budget on the UI
-  }
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-  document.addEventListener('keypress', function(event) {
-    if(event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  };
+  
+  return {
+    init: function() {
+      console.log('Application has started');
+      setupEventListeners();
     }
-  })
+  };
+  
 })(budgetController, UIController);
+
+controller.init();
